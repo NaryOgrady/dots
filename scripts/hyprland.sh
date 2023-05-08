ba86401f3706ceba5113816985dcae5eef82e0e0
NaryOgrady <josel.garza@outlook.com>
2023-05-07 19:59:50 -0600

    Add move and scroll actions

--- scripts/hyprland.sh
+++ scripts/hyprland.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+MONITOR1=("1" "3" "5" "7" "9")
+MONITOR2=("2" "4" "6" "8" "10")
 
 function move {
-    grouped = $(hyprctl activewindow -j | jq '.grouped | .[0]')
-    echo $grouped
+    grouped=$(hyprctl activewindow -j | jq '.grouped | .[0]')
 
-    if [[ $grouped != "" ]]; then
-        hyprctl dispatch moveoutofgroup
-        hyprctl dispatch movewindow $1
+    if [[ $grouped != "null" ]]; then
+        if [[ $1 == "u" || $1 == "d" ]]; then
+            hyprctl dispatch moveoutofgroup
+            exit
+        elif [ $1 == "r" ]; then
+            hyprctl dispatch changegroupactive f
+        elif [ $1 == "l" ]; then
+            hyprctl dispatch changegroupactive b
+        fi
         exit
     fi
 
@@ -14,11 +21,33 @@ function move {
     hyprctl dispatch movewindow $1
 }
 
+function scroll {
+    active_monitor=$(hyprctl activeworkspace -j | jq -r '.monitor')
+    monitor_id=$(hyprctl monitors -j | jq '.[] | select(.name == '\"$active_monitor\"') | .id')
+    active_workspace=$(hyprctl activeworkspace -j | jq '.id')
+    eval "ws_array=(\"\${MONITOR${monitor_id}[@]}\")"
+    index=0
+    for ws in "${ws_array[@]}"
+    do
+        if [ $ws == $active_workspace ]; then
+            break
+        fi
+        ((index=index+1))
+    done
+    if [ $1 == "f" ]; then
+        ((index=index+1))
+    elif [ $1 == "b" ]; then
+        ((index=index-1))
+    fi
+    new_ws="${ws_array[index]}"
+    hyprctl dispatch workspace $new_ws
+}
+
 
 if [ "$1" == "move" ]; then
     move $2
-elif [ "$1" == "network" ]; then
-    echo  "Other action"
+elif [ "$1" == "scroll" ]; then
+    scroll $2
 else
     notify-send "invalid param"
 fi
